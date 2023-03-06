db6e30a6a82015ed2f61800a2d343e56dffde04e
opedroporto <portopdr@gmail.com>
2023-03-06 20:17:04 -0300

    arquivos fronted organizados

---
+++ rifaem2/credentials/credentials.py
@@ -0,0 +1,8 @@
+CREDENTIALS = {
+    "client_id": "Client_Id_68d8fd7059501f0f1be3d9c29d7385c06dfd2ff1",
+    "client_secret": "Client_Secret_04e9c4a13d63ebbceb3640d3062506495de00fa7",
+    "sandbox": False,
+    "certificate": "rifaem2/credentials/producao-rifaem2.pem"
+}
+
+URL_PROD = "https://api-pix.gerencianet.com.br"
