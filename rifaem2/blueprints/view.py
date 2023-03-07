f6aaaf145c983a0016dec612ce2c5021326dadbc
opedroporto <portopdr@gmail.com>
2023-03-07 01:12:01 -0300

    funções da API PIX modificadas, rota /requisitarcompra definida

--- rifaem2/blueprints/view.py
+++ rifaem2/blueprints/view.py
@@ -1,94 +1,48 @@
 import rstr
 import base64
-from flask import request, render_template
 
-#from .pix.ModeloPix import ModeloPix
-from gerencianet import Gerencianet
+from flask import request, render_template
 from rifaem2.credentials import credentials
-gn = Gerencianet(credentials.CREDENTIALS)
+
+from .pix.ModeloPix import ModeloPix
+
+
 
 def init_app(app):
     @app.route("/", methods=["GET"])
     def index():
         return render_template("index.html")
 
-    '''
-        IMPLEMENTAÇÃO USANDO A CLASSSE ModeloPix com o uso do token de authenticação (antiga)
-    '''
-    '''
-    @app.route("/pix", methods=["POST"])
-    def pix():
+    @app.route("/requisitarcompra", methods=["POST"])
+    def requisitarcompra():
+        pix = ModeloPix()
+
         dados = {}
         if request.form.get('cpf'):
             dados['cpf'] = request.form.get('cpf')
         if request.form.get('nome'):
             dados['nome'] = request.form.get('nome')
-        if request.form.get('valor'):
-            dados['valor'] = request.form.get('valor')
+        if request.form.get('telefone'):
+            dados['telefone'] = request.form.get('telefone')
+        if request.form.get('email'):
+            dados['email'] = request.form.get('email')
+        if request.form.get('preco'):
+            dados['preco'] = request.form.get('preco')
 
-        dados['txid'] = rstr.xeger(r'^[a-zA-Z0-9]{26,35}$')
+        txid = rstr.xeger(r'^[a-zA-Z0-9]{26,35}$')
 
-        payload = {
-            "txid": dados['txid'],
-            "calendario": {
-                "expiracao": 300
-            },
-            "devedor": {
-                "cpf": dados['cpf'],
-                "nome": dados['nome']
-            },
-            "valor": {
-                "original": dados['valor']
-            },
-            "chave": "e5ab195e-c2a5-42db-9ce1-3841e6e22615",
-            "solicitacaoPagador": "Obrigado por comprar! Conteúdo da compra:"
-        }
-        txid = payload.pop("txid")
+        response = pix.gerapix({
+            'preco': dados['preco'],
+            'txid': txid
+        })
 
-        pix_model = ModeloPix()
-        image64 = pix_model.create_charge(txid, payload)
+        loc_id = response['loc']['id']
 
-        return f'<img src="{image64}">'
-    '''
+        qrcode = pix.geraqrcode(loc_id)
 
-    @app.route("/pix", methods=["POST"])
-    def pix():
-        dados = {}
-        if request.form.get('cpf'):
-            dados['cpf'] = request.form.get('cpf')
-        if request.form.get('nome'):
-            dados['nome'] = request.form.get('nome')
-        if request.form.get('valor'):
-            dados['valor'] = request.form.get('valor')
+        '''
+        verificacao pix
+        response = pix.verificapix(txid)
+        '''
 
-        dados['txid'] = rstr.xeger(r'^[a-zA-Z0-9]{26,35}$')
-
-        params = {
-            'txid': dados['txid']
-        }
-
-        body = {
-            'calendario': {
-                'expiracao': 300
-            },
-            'devedor': {
-                'cpf': dados['cpf'],
-                'nome': dados['nome']
-            },
-            'valor': {
-                'original': dados['valor']
-            },
-            'chave': 'e5ab195e-c2a5-42db-9ce1-3841e6e22615',
-            'solicitacaoPagador': 'Cobrança dos serviços prestados.'
-        }
-
-        response =  gn.pix_create_charge(params=params,body=body)
-
-        params = {
-            'id': response['loc']['id']
-        }
-
-        response = gn.pix_generate_QRCode(params=params)
-        base64 = response['imagemQrcode']
-
-        return f'<img src="{base64}">'
+        return qrcode
