3553b747c1bace241770c3f9e4ea42b4c6c8105c
opedroporto <portopdr@gmail.com>
2023-03-19 23:29:11 -0300

    lista de pedidos arrumada

--- rifaem2/blueprints/pix/api.py
+++ rifaem2/blueprints/pix/api.py
@@ -39,5 +39,6 @@ def listaPedidos(pedidosTxid):
 
     resposta = requests.post(url, json=data, headers=headers)
     resposta = resposta.json()['result']
+    resposta.reverse()
 
     return resposta
