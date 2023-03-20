69069e7a7be746aa2d9e39ba4c5e50590b019137
guiberg01 <feijaoprato@gmail.com>
2023-03-20 12:50:44 -0300

    Melhorias na responsividade

--- rifaem2/static/js/menuBurguer.js
+++ rifaem2/static/js/menuBurguer.js
@@ -4,11 +4,13 @@ const menuAside = document.querySelector(".menu-aside")
 btnBurguer.addEventListener("click", function () {
     btnBurguer.classList.toggle("on");
     menuAside.classList.toggle("on");
+	document.documentElement.classList.toggle("on");
 })
 
 window.addEventListener("resize", (e) => {
     if (window.innerWidth > 860){ 
         btnBurguer.classList.remove("on");
         menuAside.classList.remove("on");
+		document.documentElement.classList.remove("on");
     };
 });
