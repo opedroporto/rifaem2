00a63aaa9fa756510f9016f579ba8c985d4025ff
guiberg01 <feijaoprato@gmail.com>
2023-03-20 17:11:06 -0300

    Melhorias de responsividade e estilização da aba Pedidos

--- rifaem2/static/js/menuBurguer.js
+++ rifaem2/static/js/menuBurguer.js
@@ -1,11 +1,17 @@
-const btnBurguer = document.querySelector(".menu-burguer")
-const menuAside = document.querySelector(".menu-aside")
+const btnBurguer = document.querySelector(".menu-burguer");
+const menuAside = document.querySelector(".menu-aside");
+const rifasLi = document.getElementById("rifasLink");
 
 btnBurguer.addEventListener("click", function () {
     btnBurguer.classList.toggle("on");
     menuAside.classList.toggle("on");
 	document.documentElement.classList.toggle("on");
-})
+});
+rifasLi.addEventListener("click", function () {
+		btnBurguer.classList.remove("on");
+        menuAside.classList.remove("on");
+		document.documentElement.classList.remove("on");
+});
 
 window.addEventListener("resize", (e) => {
     if (window.innerWidth > 860){ 
