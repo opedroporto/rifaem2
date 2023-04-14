547af348a79dbdd0da40be0ce7f882ac5d0a1cff
guiberg01 <feijaoprato@gmail.com>
2023-04-14 20:49:30 -0300

    Alterações no pix modal e rifas

--- rifaem2webapp/application/static/js/menuBurguer.js
+++ rifaem2webapp/application/static/js/menuBurguer.js
@@ -1,6 +1,7 @@
 const btnBurguer = document.querySelector(".menu-burguer");
 const menuAside = document.querySelector(".menu-aside");
 const rifasLi = document.getElementById("rifasLink");
+const header = document.querySelector("header");
 
 btnBurguer.addEventListener("click", function () {
     const aberto = btnBurguer.getAttribute('aria-expanded') === "true";
@@ -12,29 +13,39 @@ function menuOpen() {
 	menuAside.setAttribute("data-state", "opened");
 	btnBurguer.classList.add("on");
 	menuAside.classList.add("on");
-	document.documentElement.classList.add("on");
 };
 function menuClose() {
 	btnBurguer.setAttribute("aria-expanded", "false");
 	menuAside.setAttribute("data-state", "closing");
 	btnBurguer.classList.remove("on");
 	menuAside.classList.remove("on");
-	document.documentElement.classList.remove("on");
 	menuAside.addEventListener("animationend", () => {
 		menuAside.setAttribute("data-state", "closed");
 	}, {once: true});
 };
 
 rifasLi.addEventListener("click", function () {
-		btnBurguer.classList.remove("on");
-        menuAside.classList.remove("on");
-		document.documentElement.classList.remove("on");
+	btnBurguer.classList.remove("on");
+       menuAside.classList.remove("on");
+	btnBurguer.setAttribute("aria-expanded", "false");
+	menuAside.setAttribute("data-state", "closing");
+	menuAside.addEventListener("animationend", () => {
+		menuAside.setAttribute("data-state", "closed");
+	}, {once: true});
 });
 
 window.addEventListener("resize", (e) => {
-    if (window.innerWidth > 860){ 
+    if (window.innerWidth > 860 && btnBurguer.getAttribute('aria-expanded') === "true"){ 
         btnBurguer.classList.remove("on");
         menuAside.classList.remove("on");
-		document.documentElement.classList.remove("on");
+		btnBurguer.setAttribute("aria-expanded", "false");
+		menuAside.setAttribute("data-state", "closing");
+		menuAside.addEventListener("animationend", () => {
+			menuAside.setAttribute("data-state", "closed");
+		}, {once: true});
     };
+});
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 280);
 });
