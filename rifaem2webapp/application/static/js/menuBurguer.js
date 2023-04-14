7da057eaf05d046e1949e5ac1ea7811090b36ac1
guiberg01 <feijaoprato@gmail.com>
2023-04-13 23:10:53 -0300

    Responsividade rifas completa e corrigido bug de tela

--- rifaem2webapp/application/static/js/menuBurguer.js
+++ rifaem2webapp/application/static/js/menuBurguer.js
@@ -3,10 +3,28 @@ const menuAside = document.querySelector(".menu-aside");
 const rifasLi = document.getElementById("rifasLink");
 
 btnBurguer.addEventListener("click", function () {
-    btnBurguer.classList.toggle("on");
-    menuAside.classList.toggle("on");
-	document.documentElement.classList.toggle("on");
+    const aberto = btnBurguer.getAttribute('aria-expanded') === "true";
+	if(aberto ? menuClose() : menuOpen());
 });
+
+function menuOpen() {
+	btnBurguer.setAttribute("aria-expanded", "true");
+	menuAside.setAttribute("data-state", "opened");
+	btnBurguer.classList.add("on");
+	menuAside.classList.add("on");
+	document.documentElement.classList.add("on");
+};
+function menuClose() {
+	btnBurguer.setAttribute("aria-expanded", "false");
+	menuAside.setAttribute("data-state", "closing");
+	btnBurguer.classList.remove("on");
+	menuAside.classList.remove("on");
+	document.documentElement.classList.remove("on");
+	menuAside.addEventListener("animationend", () => {
+		menuAside.setAttribute("data-state", "closed");
+	}, {once: true});
+};
+
 rifasLi.addEventListener("click", function () {
 		btnBurguer.classList.remove("on");
         menuAside.classList.remove("on");
